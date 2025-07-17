d8a8f75abba8e1aa1ee94d6bd4affbcd344c6cf5
CollierTR <tristancollier777@gmail.com>
2025-07-17 15:44:23 -0500

    update: changed defualt colorscheme

--- nvim/init.lua
+++ nvim/init.lua
@@ -169,8 +169,6 @@ require("lazy").setup({
 		"atmosuwiryo/vim-winteriscoming",
 	},
 
-	{ "EdenEast/nightfox.nvim" },
-
 	{ "rrethy/vim-illuminate" },
 
 	{ "numToStr/comment.nvim" },
@@ -781,14 +779,11 @@ require("lazy").setup({
 
 	{ -- You can easily change to a different colorscheme.
 		-- Change the name of the colorscheme plugin below, and then -- change the command in the config to whatever the name of that colorscheme is. -- -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-		"easteden/nightfox.nvim",
-		--"folke/tokyonight.nvim",
+		"folke/tokyonight.nvim",
 		priority = 1000, -- Make sure to load this before all the other start plugins.
 		init = function() -- Load the colorscheme here. -- Like many other themes, this one has different styles, and you could load -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			vim.cmd.colorscheme("carbonfox") -- You can configure highlights by doing something like: vim.cmd.hi("Comment gui=none")
-			--vim.cmd.colorscheme("tokyonight") -- You can configure highlights by doing something like: vim.cmd.hi("Comment gui=none")
+			vim.cmd.colorscheme("tokyonight") -- You can configure highlights by doing something like: vim.cmd.hi("Comment gui=none")
 			--vim.cmd.colorscheme("WinterIsComing-dark-blue-color-theme") -- You can configure highlights by doing something like: vim.cmd.hi("Comment gui=none")
-			-- vim.cmd.colorscheme("Carbonfox") -- You can configure highlights by doing something like: vim.cmd.hi("Comment gui=none")
 		end,
 	},
 	-- Highlight todo, notes, etc in comments
