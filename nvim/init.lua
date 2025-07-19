be3b96fab0051aab2650fa67c43d121ccb47b326
CollierTR <tristancollier777@gmail.com>
2025-07-18 20:49:17 -0500

    update: updated to the prefered schema of conform plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -661,7 +661,8 @@ require("lazy").setup({
 				--
 				-- You can use a sub-list to tell conform to run *until* a formatter
 				-- is found.
-				javascript = { { "prettierd", "prettier" } },
+				--javascript = { { "prettierd", "prettier" } },
+				javascript = { "prettier" },
 			},
 		},
 	},
