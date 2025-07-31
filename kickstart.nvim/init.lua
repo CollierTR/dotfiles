8563dc30f072dd623f3e36a2754c2c7a79fa5812
CollierTR <tristancollier777@gmail.com>
2025-07-31 13:38:23 -0500

    feat: add render-markdown and bullets.nvim plugins

--- kickstart.nvim/init.lua
+++ kickstart.nvim/init.lua
@@ -214,6 +214,20 @@ require('lazy').setup({
   },
 
   {
+    'MeanderingProgrammer/render-markdown.nvim',
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
+
+  {
+    'bullets-vim/bullets.vim',
+  },
+
+  {
     'otavioschwanck/arrow.nvim',
     dependencies = {
       { 'nvim-tree/nvim-web-devicons' },
