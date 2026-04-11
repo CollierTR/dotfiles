ca5f516922c10b1688ed1b08a415a4ec1def7941
CollierTR <tristancollier777@gmail.com>
2026-04-11 14:43:49 -0500

    update (nvim): add gruvbox theme

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -2,13 +2,14 @@ return {
 	{ "rebelot/kanagawa.nvim" },
 	{ "luisiacc/the-matrix.nvim" },
 	{ "AlexvZyl/nordic.nvim" },
+	{ "ellisonleao/gruvbox.nvim" },
 	{
 		"folke/tokyonight.nvim",
 		lazy = false,
 		priority = 1000,
 		opts = {},
 		init = function()
-			vim.cmd("colorscheme thematrix")
+			vim.cmd("colorscheme gruvbox")
 		end,
 	},
 }
