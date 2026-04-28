56cffe0f0cde7414b0104230cba304b938544da3
CollierTR <tristancollier777@gmail.com>
2026-04-28 13:14:07 -0500

    refactor (bash): make nvm.sh safe

--- bash/.bashrc.d/nvm.sh
+++ bash/.bashrc.d/nvm.sh
@@ -2,10 +2,18 @@ load_nvmrc() {
   local nvmrc_path
   nvmrc_path="$(nvm_find_nvmrc)"
 
-  if [ -n "$nvmrc_path" ]; then
-    nvm use
+  if [ "$nvmrc_path" != "$NVMRC_PATH" ]; then
+    NVMRC_PATH="$nvmrc_path"
+
+    if [ -n "$nvmrc_path" ]; then
+      nvm use
+    else
+      nvm use default
+    fi
   fi
 }
 
 # Run when directory changes
-PROMPT_COMMAND="load_nvmrc;$PROMPT_COMMAND"
+if [[ "$PROMPT_COMMAND" != *load_nvmrc* ]]; then
+  PROMPT_COMMAND="load_nvmrc;${PROMPT_COMMAND}"
+fi
