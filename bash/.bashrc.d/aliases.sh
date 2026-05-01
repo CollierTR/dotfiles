e9f416955e3014b27e40f8721e99267a381bc1e5
CollierTR <tristancollier777@gmail.com>
2026-05-01 10:46:11 -0500

    update (bash): pipe gl command into toc command

--- bash/.bashrc.d/aliases.sh
+++ bash/.bashrc.d/aliases.sh
@@ -38,7 +38,8 @@ alias la='ls -A'
 alias fetch='clear && fastfetch --logo none'
 
 # View the Git log.
-alias gl='git --no-pager log --oneline --graph --all'
+alias gl='git --no-pager log --graph --oneline --all --color | tac'
+
 
 # Clear the Terminal
 alias cls='clear'
