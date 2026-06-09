e87bd59f94566871b46eed4a93a5c16dd635845a
CollierTR <tristancollier777@gmail.com>
2026-06-09 13:47:03 -0500

    update (nvim): auto resize nvim-tree

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -1,25 +1,30 @@
 -- NVIM tree
 
 return {
-  "nvim-tree/nvim-tree.lua",
-  version = "*",
-  lazy = false,
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  keys = {
-    { "<leader>e", "<cmd>NvimTreeToggle<CR>", desc = "Toggle file explorer" },
-  },
-  config = function()
-    require("nvim-tree").setup({
-      view = {
-        side = "right",
-        width = 40,
-      },
-      filters = {
-        git_ignored = false,
-        dotfiles = false,
-      },
-    })
-  end,
+	"nvim-tree/nvim-tree.lua",
+	version = "*",
+	lazy = false,
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	keys = {
+		{ "<leader>e", "<cmd>NvimTreeToggle<CR>", desc = "Toggle file explorer" },
+	},
+	config = function()
+		require("nvim-tree").setup({
+			view = {
+				side = "right",
+
+				-- Dynamically resizes the tree window based on longest file name
+				adaptive_size = true,
+
+				-- Hard coded size
+				-- width = 40,
+			},
+			filters = {
+				git_ignored = false,
+				dotfiles = false,
+			},
+		})
+	end,
 }
