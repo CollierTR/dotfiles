236c93664d271a78bb880118575bdc63382ed643
CollierTR <tristancollier777@gmail.com>
2026-06-17 13:28:06 -0500

    update (nvim): remove old config backup

--- nvim/.config/backup-nvim/defualt-lua/plugins/theme.lua
+++
@@ -1,1 +0,0 @@
-/home/tristan/.config/omarchy/current/theme/neovim.lua
--- nvim/.config/backup-nvim/lua/plugins/nvim-autopairs.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "windwp/nvim-autopairs", -- auto pairs tags, brackets, ect..
-  event = "InsertEnter",
-  config = true,
-  -- use opts = {} for passing setup options
-  -- this is equalent to setup({}) function
-}
